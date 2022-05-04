3c76a1dbcbcd149ae04813a57476ea85ee43ac0d
absya23 <thachnc2002@gmail.com>
2022-05-04 16:20:59 +0700

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,10 @@
+// import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import "./App.scss";
+
+function App() {
+  return <div className="App">Hello word</div>;
+}
+
+export default App;
